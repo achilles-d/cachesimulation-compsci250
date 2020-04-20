11b70c450dbe9f23430a719e40994fa824a04cb0
Achilles Dabrowski <achilles.dabrowski@gmail.com>
2020-04-20 15:23:52 -0400

    Added initial file reading scheme to virt2phys

--- virt2phys.c
+++ virt2phys.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc){
-    
+int main(int argc, char *argv[]){
+    //Load arguments 
+    pgFile_ptr = argv[0];
+    virtAdd_ptr = argv[1];
+    //Load page file
+    FILE * pgFile = fopen(*pgFile_ptr, "r+");
+    //Load page file heading fields
+    int addBits = 0;
+    int pageSize = 0;
+    fscanf(pgFile, "%d %d", addBits, pageSize);
+
 }
