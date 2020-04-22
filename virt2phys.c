647633adb0bd5d9222ae77fa1724c95cd66beab9
Achilles Dabrowski <achilles.dabrowski@gmail.com>
2020-04-22 17:56:52 -0400

    Passed all tests and removed commented code for testing

--- virt2phys.c
+++ virt2phys.c
@@ -7,28 +7,6 @@ int log2(int n) { int r=0;
     return r;
 }
 
-void decToBinary(int n) 
-{ 
-    // array to store binary number 
-    int binaryNum[32]; 
-  
-    // counter for binary array 
-    int i = 0; 
-    while (n > 0) { 
-  
-        // storing remainder in binary array 
-        binaryNum[i] = n % 2; 
-        n = n / 2; 
-        i++; 
-    } 
-  
-    // printing binary array in reverse order 
-    for (int j = i - 1; j >= 0; j--) 
-        printf("%d", binaryNum[j]); 
-
-    printf("\n");
-} 
-
 int main(int argc, char *argv[]){
     //Load virtual address
     int virtAddr; 
@@ -39,11 +17,6 @@ int main(int argc, char *argv[]){
     int virtAddrSize = 0;
     int pageSize = 0;
     fscanf(pgFile, "%d %d", &virtAddrSize, &pageSize);
-    fscanf(pgFile, "%*[^\n]\n");
-    /*
-    printf("Page size:\n");
-    printf("%d\n", log2(pageSize));
-    */
     //Compute the page offset size and the VPN size
     int pgOffsetSize = log2(pageSize);
     //Create and use bit masks to extract page offset and VPN 
@@ -51,29 +24,16 @@ int main(int argc, char *argv[]){
     
     int pgOffsetBits = virtAddr & pgOffsetMask; 
     int vpnBits = virtAddr >> pgOffsetSize;
-    /*
-    printf("Virtual address:\n");
-    decToBinary(virtAddr);
-    printf("Page offset bits:\n");
-    decToBinary(pgOffsetBits);
-    printf("VPN:\n");
-    decToBinary(vpnBits);
-    */
     int ppn = 0; 
     //Loop to correct location and read PPN
     int i; 
     for(i = 0; i < vpnBits + 1; i++){
-        if(i == (vpnBits)){
-            fscanf(pgFile, "%d", &ppn);
-        }
-        else{
-            fscanf(pgFile, "%*[^\n]\n");
-        }
+        fscanf(pgFile, "%d", &ppn);
     }
-    fclose(pgFile);
     //Print the result
-    if(ppn == -1)
+    if(ppn == -1){
         printf("PAGEFAULT\n");
+    }
     else{
         //"Concatenate" the PPN and page offset 
         ppn = ppn << pgOffsetSize; 
